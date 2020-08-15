5be8cca8ad61dd1e8579354f8ae8838acf6c44f3
GianluigiMemoli <g.memoli98@gmail.com>
2020-08-15 15:39:23 +0200

    rady to deploy

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -14,17 +14,17 @@ class Scraper:
     BODY = 'selftext'
     TITLE = 'title'
 
-    def __init__(self, keywords, match_in, must_match: [], output_stream=sys.stdout):
-        self.output_stream = output_stream
+    def __init__(self, keywords, match_in, must_match: []):
         if len(match_in) == 0:
             raise Exception("Must specify at least one value for match_in from Scraper.TITLE and Scraper.BODY")
         if len(keywords) == 0:
             raise Exception("Must specify at least one value for keyword")
+
         self.must_match = must_match
         self.keywords = [x.lower() for x in keywords]
         self.match_in = match_in
         self.listeners = []
-        print("matchin: {}".format(self.match_in), file=self.output_stream, flush=True)
+        print("matchin: {}".format(self.match_in))
 
     def match_pattern(self, new_post: praw.reddit.Submission):
 
@@ -32,17 +32,17 @@ class Scraper:
         for to_match in self.match_in:
             raw_text = getattr(new_post, to_match)
             texts.append(raw_text.lower())
-        print("texts {}\n".format(texts), self.output_stream)
+        print("texts {}\n".format(texts))
         # If no must_match is specified it's given for satisfied
         must_matched = len(self.must_match) == 0
 
         for must_matching in self.must_match:
             for text in texts:
                 if re.match(must_matching, text, re.IGNORECASE) is not None:
-                    print("regex matched\n", self.output_stream)
+                    print("regex matched\n")
                     must_matched = True
                 else:
-                    print("regex: {} not matched on {}\n".format(must_matching, text), file=self.output_stream)
+                    print("regex: {} not matched on {}\n".format(must_matching, text))
         if not must_matched:
             return False
 
@@ -51,16 +51,15 @@ class Scraper:
             for keyword in self.keywords:
                 if keyword in text:
                     keyword_matched = True
-                    print("matched kw: {} on: {}\n".format(keyword, text), file=self.output_stream)
+                    print("matched kw: {} on: {}\n".format(keyword, text))
                 else:
-                    print("not matched kw: {} on: {}\n".format(keyword, text), file=self.output_stream)
+                    print("not matched kw: {} on: {}\n".format(keyword, text))
         return keyword_matched and must_matched
 
-
-    def scraping(self):
+    def analyze_stream(self):
         reddit = praw.Reddit(**config)
         while True:
-            for submission in reddit.subreddit('forhire').stream.submissions(skip_existing=True):
+            for submission in reddit.subreddit('test').stream.submissions(skip_existing=True):
                 if self.match_pattern(submission):
                     self.on_new_post_match(submission)
 
@@ -72,6 +71,9 @@ class Scraper:
         self.listeners.remove(listener)
 
     def on_new_post_match(self, post):
+        print("I'm a listenr who got")
+        print()
+        print(post)
         for listener in self.listeners:
             listener.on_new_post(post)
 
--- job_bot.py
+++ job_bot.py
@@ -1,5 +1,7 @@
+import datetime
 import os
 import logging
+import configparser
 import threading
 import telegram
 from telegram.ext import Updater, CommandHandler
@@ -8,22 +10,60 @@ from Scraper.scraper import Scraper
 TOKEN = '1231856261:AAEP2kvBYIPg2YwCXaCnEeidiQF7cYdA-Lw'
 interested_clients = set()
 
-kwords = ["js", "javascript", "java", "sql", "mysql", "c", "golang", "python", "database", "angular", "react", "desktop", "site", "website", "html", "css", "developer", "dev", "programmer", "code", "coder", "cloud", "bootstrap", "linux", "developers", "node", "node.js"]
+kwords = [
+    "js",
+    "javascript",
+    "java",
+    "sql",
+    "mysql",
+    "c",
+    "golang",
+    "python",
+    "database",
+    "angular",
+    "react",
+    "desktop",
+    "site",
+    "website",
+    "html",
+    "css",
+    "developer",
+    "dev",
+    "programmer",
+    "code",
+    "coder",
+    "cloud",
+    "bootstrap",
+    "linux",
+    "developers",
+    "node",
+    "node.js"
+]
 
-file = open("logs", "a+")
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+def write_logs(text):
+    filename = "{}/{}".format("logs",str(datetime.date.today()))
+    with open(filename, "a+") as log_file:
+        time = str(datetime.datetime.now())
+        log_file.write("[{}] {}\n".format(time.split('.')[0], text))
+
+
 
 def start(update, ctx):
     interested_clients.add(update.effective_chat.id)
     ctx.bot.send_message(chat_id=update.effective_chat.id, text="Now you'll start reiceve messages for each post matches your needings")
 
 def get_logs(update, ctx):
-    global file
-    file.close()
-    file = open("logs", "a+")
-    log_content = file.read()
-    ctx.bot.send_message(chat_id=update.effective_chat.id,
-                         text=log_content if len(log_content) > 0 else "nope")
-    file = open("logs", "a+")
+    files = os.listdir("logs")
+    print("chiesti logs")
+    for file in files:
+        print("trovat: {}".format(file))
+        ofile = open("logs/"+file, 'rb')
+        ctx.bot.send_document(chat_id=update.effective_chat.id,
+                         document=ofile)
+        ofile.close()
 class bot:
 
     def __init__(self):
@@ -35,20 +75,19 @@ class bot:
         self.dispatcher.add_handler(CommandHandler('logs', get_logs))
 
         self.x = threading.Thread(target=self.async_scraping, daemon=True)
-        self.updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-        self.updater.bot.setWebhook('https://redjobot.herokuapp.com/' + TOKEN)
         self.x.start()
+        write_logs("started")
+        self.updater.start_polling()
+
 
     def async_scraping(self):
-
-        self.scraper = Scraper(kwords, [Scraper.TITLE, Scraper.BODY], ['^\s*\[\s*HIRING\s*\]'], output_stream=file)
+        self.scraper = Scraper(kwords, [Scraper.TITLE, Scraper.BODY], ['^\s*\[\s*COSA\s*\]'])
         self.scraper.add_listener(self)
-        self.scraper.scraping()
+        self.scraper.analyze_stream()
 
     def on_new_post(self, post):
         for id in interested_clients:
             self.bot.send_message(chat_id=id, text="<b>{}</b>\n{}\n\n{}".format(post.title, post.selftext, post.url), parse_mode=telegram.ParseMode.HTML)
+
 
 bot()
